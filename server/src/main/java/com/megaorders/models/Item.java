069153ccda8aec61a7a5544e5344fa093eb4ef40
divanshu3451 <awesomedev3451@gmail.com>
2025-08-31 12:30:53 +0530

    fix: created Item table to maintain individual items which are costly like electronics, and added DeliveryStatus for an item

---
+++ server/src/main/java/com/megaorders/models/Item.java
@@ -0,0 +1,7 @@
+package com.megaorders.models;
+
+public class Item {
+    private Long id;
+    private String serialNumber;
+
+}
---
+++ server/src/main/java/com/megaorders/models/enums/DeliveryStatus.java
@@ -0,0 +1,9 @@
+package com.megaorders.models.enums;
+
+public enum DeliveryStatus {
+    NOT_DISPATCHED,
+    DISPATCHED,
+    NEAREST_HUB,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+}
